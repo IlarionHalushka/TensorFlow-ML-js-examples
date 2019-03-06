12fdb0fae1da9636fb564dae9ad1509568ec2488
IlarionHalushka <hilarion.galushka@gmail.com>
2019-03-06 22:12:35 +0200

    add neural network with genetics

---
+++ tensorflow/lab6-classification/classification.py
@@ -0,0 +1,37 @@
+# another source with genetics https://github.com/vanhooser/TF-Genetic
+# source wtih genetics https://github.com/amirdeljouyi/Genetic-Algorithm-on-K-Means-Clustering
+# source of this file code  https://www.tensorflow.org/api_docs/python/tf/contrib/factorization/KMeansClustering
+
+import numpy as np
+import tensorflow as tf
+
+num_points = 100
+dimensions = 8
+points = np.random.uniform(0, 1000, [num_points, dimensions])
+
+def input_fn():
+  return tf.train.limit_epochs(
+      tf.convert_to_tensor(points, dtype=tf.float32), num_epochs=1)
+
+num_clusters = 5
+kmeans = tf.contrib.factorization.KMeansClustering(
+    num_clusters=num_clusters, use_mini_batch=False)
+
+# train
+num_iterations = 10
+previous_centers = None
+for _ in xrange(num_iterations):
+  kmeans.train(input_fn)
+  cluster_centers = kmeans.cluster_centers()
+  if previous_centers is not None:
+    print 'delta:', cluster_centers - previous_centers
+  previous_centers = cluster_centers
+  print 'score:', kmeans.score(input_fn)
+print 'cluster centers:', cluster_centers
+
+# map the input points to their clusters
+cluster_indices = list(kmeans.predict_cluster_index(input_fn))
+for i, point in enumerate(points):
+  cluster_index = cluster_indices[i]
+  center = cluster_centers[cluster_index]
+  print 'point:', point, 'is in cluster', cluster_index, 'centered at', center

240e6535c7fe17f0e2e773c4f698507f1aef7dfe
IlarionHalushka <hilarion.galushka@gmail.com>
2019-03-12 22:27:02 +0200

    install prettier

--- tensorflow/imageClassification/node.js
+++ tensorflow/imageClassification/node.js
@@ -1,4 +1,4 @@
-const tf = require('@tensorflow/tfjs')
+const tf = require('@tensorflow/tfjs');
 const mobilenet = require('@tensorflow-models/mobilenet');
 const fs = require('fs');
 const jpeg = require('jpeg-js');
@@ -7,52 +7,52 @@ require('@tensorflow/tfjs-node');
 const NUMBER_OF_CHANNELS = 3;
 
 const readImage = path => {
-	const buf = fs.readFileSync(path);
-	return jpeg.decode(buf, true);
+  const buf = fs.readFileSync(path);
+  return jpeg.decode(buf, true);
 };
 
 const imageByteArray = (image, numChannels) => {
-	const pixels = image.data
-	const numPixels = image.width * image.height;
-	const values = new Int32Array(numPixels * numChannels);
+  const pixels = image.data;
+  const numPixels = image.width * image.height;
+  const values = new Int32Array(numPixels * numChannels);
 
-	for (let i = 0; i < numPixels; i++) {
-		for (let channel = 0; channel < numChannels; ++channel) {
-			values[i * numChannels + channel] = pixels[i * 4 + channel];
-		}
-	}
+  for (let i = 0; i < numPixels; i++) {
+    for (let channel = 0; channel < numChannels; ++channel) {
+      values[i * numChannels + channel] = pixels[i * 4 + channel];
+    }
+  }
 
-	return values;
+  return values;
 };
 
 const imageToInput = (image, numChannels) => {
-	const values = imageByteArray(image, numChannels)
-	const outShape = [image.height, image.width, numChannels];
-	return tf.tensor3d(values, outShape, 'int32');
+  const values = imageByteArray(image, numChannels);
+  const outShape = [image.height, image.width, numChannels];
+  return tf.tensor3d(values, outShape, 'int32');
 };
 
 const loadModel = async path => {
-	const mn = new mobilenet.MobileNet(1, 1);
-	mn.path = `file://mobile-net/model.json`;
-	await mn.load();
-	return mn
+  const mn = new mobilenet.MobileNet(1, 1);
+  mn.path = `file://mobile-net/model.json`;
+  await mn.load();
+  return mn;
 };
 
 const classify = async (model, path) => {
-	console.time()
-	const image = readImage(path)
-	const input = imageToInput(image, NUMBER_OF_CHANNELS)
+  console.time();
+  const image = readImage(path);
+  const input = imageToInput(image, NUMBER_OF_CHANNELS);
 
-	const  mn_model = await loadModel(model);
+  const mn_model = await loadModel(model);
 
-	const predictions = await mn_model.classify(input)
-	console.timeEnd()
+  const predictions = await mn_model.classify(input);
+  console.timeEnd();
 
-	console.log('classification results:', predictions)
+  console.log('classification results:', predictions);
 };
 
 // if (process.argv.length !== 4) throw new Error('incorrect arguments: node script.js <MODEL> <IMAGE_FILE>')
 // classify(process.argv[2], process.argv[3])
-classify(process.argv[2], process.argv[2])
+classify(process.argv[2], process.argv[2]);
 
 // to run classification: node node.js ant.jpg

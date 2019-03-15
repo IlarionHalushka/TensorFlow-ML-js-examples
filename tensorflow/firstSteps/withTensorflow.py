88c83450e017714a917e89474264d00263d79240
IlarionHalushka <hilarion.galushka@gmail.com>
2019-03-15 23:16:38 +0200

    google dvelopers guide first steps with tensorflow

---
+++ tensorflow/firstSteps/withTensorflow.py
@@ -0,0 +1,32 @@
+# https://colab.research.google.com/notebooks/mlcc/first_steps_with_tensor_flow.ipynb?utm_source=mlcc&utm_campaign=colab-external&utm_medium=referral&utm_content=firststeps-colab&hl=ru#scrollTo=7UwqGbbxP53O
+
+from __future__ import print_function
+
+import math
+
+from IPython import display
+from matplotlib import cm
+from matplotlib import gridspec
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import tensorflow as tf
+from tensorflow.python.data import Dataset
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.1f}'.format
+
+california_housing_dataframe = pd.read_csv("https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv", sep=",")
+
+california_housing_dataframe.describe()
+
+# Define the input feature: total_rooms.
+my_feature = california_housing_dataframe[["total_rooms"]]
+
+# Configure a numeric feature column for total_rooms.
+feature_columns = [tf.feature_column.numeric_column("total_rooms")]
+
+# Define the label.
+targets = california_housing_dataframe["median_house_value"]

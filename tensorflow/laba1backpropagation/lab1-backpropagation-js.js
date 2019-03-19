9d7e896b15e36f30f9594100c2a85a77b5030253
IlarionHalushka <hilarion.galushka@gmail.com>
2019-03-19 23:22:42 +0200

    adjust lab1 data according to requirements

--- tensorflow/laba1backpropagation/lab1-backpropagation-js.js
+++ tensorflow/laba1backpropagation/lab1-backpropagation-js.js
@@ -3,11 +3,10 @@ import "@tensorflow/tfjs-node"
 import dataTesting from "./dataTesting"
 import dataTraining from "./dataTraining"
 
-// VIDEO ON YOUTUBE similar to this code but iris https://www.youtube.com/watch?v=XdErOpUzupY
-// google machine learning course https://codelabs.developers.google.com/codelabs/neural-tensorflow-js/index.html?index=..%2F..index#5
+import * as tfVis from '@tensorflow/tfjs-vis';
 
-// function: Math.LN10(Math.abs(Math.cos(x1)) + Math.tan(x2) - (1 / Math.tan(x3) ))
-// d1 = ln(|cos(x1)|)+tg(x2)-ctg(x3)
+
+// google machine learning course https://codelabs.developers.google.com/codelabs/neural-tensorflow-js/index.html?index=..%2F..index#5
 
 // 5,7,8
 
@@ -18,16 +17,18 @@ import dataTraining from "./dataTraining"
 // const output = [];
 // const x1x2x3training = [];
 //
-// // const modelingFunction = (x1, x2, x3) => Math.log(Math.abs(Math.cos(x1)) + Math.tan(x2) - (1 / Math.tan(x3) ));
-// // const modelingFunction = (x1, x2, x3) => Math.log(Math.abs(Math.cos(x1)) + Math.tan(x2));
+
 // const modelingFunction = (x1, x2, x3) => x1 + x2 + x3;
+// //
 //
-// for (let i = 0; i < 1000; i++) {
-//   output.push(modelingFunction(x1initial + i, x2initial + i, x3initial + i));
-//   x1x2x3training.push([x1initial + i, x2initial + i, x3initial + i]);
+// for (let i = 0; i < 27; i++) {
+//   output.push(modelingFunction(dataTraining.inputs[i][0], dataTraining.inputs[i][1], dataTraining.inputs[i][2]));
+//   // x1x2x3training.push([x1initial + i, x2initial + i, x3initial + i]);
 // }
-//
+// //
 // console.log(output.toString())
+
+console.log('HERE')
 
 const trainingData = tf.tensor2d(dataTraining.inputs);
 const outputData = tf.tensor1d(dataTraining.outputs);
@@ -43,7 +44,6 @@ const testingOutputData = tf.tensor1d(dataTesting.outputs);
   }));
   await model.add(tf.layers.dense({
     inputShape: [3],
-    activation: "relu",
     units: 1,
   }));
 
@@ -53,9 +53,21 @@ const testingOutputData = tf.tensor1d(dataTesting.outputs);
   });
 
 // // train/fit our network
-  await model.fit(trainingData, outputData, {epochs: 25})
+  await model.fit(trainingData, outputData, {epochs: 100})
     .then((history) => {
-      model.predict(testingData).print()
+      return model.predict(testingData).print()
+
+    }).then(predictions => {
+      console.log(predictions);
+      tfVis.visor().surface({ name: 'My First Surface', tab: 'Input Data' });
+      tfVis.show.history(
+        {
+          name: 'Training History1',
+          tab: 'Training1',
+        },
+        predictions,
+        ['loss']
+      );
     });
 })();
 

d43c3e9c59c5b2f012bb92fde45dcdb19461f9fc
IlarionHalushka <hilarion.galushka@gmail.com>
2019-03-20 21:53:57 +0200

    clean up the code

--- tensorflow/laba1backpropagation/lab1-backpropagation-js.js
+++ tensorflow/laba1backpropagation/lab1-backpropagation-js.js
@@ -7,28 +7,6 @@ import * as tfVis from '@tensorflow/tfjs-vis';
 
 // google machine learning course https://codelabs.developers.google.com/codelabs/neural-tensorflow-js/index.html?index=..%2F..index#5
 
-// 5,7,8
-
-// const x1initial = 5;
-// const x2initial = 7;
-// const x3initial = 8;
-//
-// const output = [];
-// const x1x2x3training = [];
-//
-
-// const modelingFunction = (x1, x2, x3) => x1 + x2 + x3;
-// //
-//
-// for (let i = 0; i < 27; i++) {
-//   output.push(modelingFunction(dataTraining.inputs[i][0], dataTraining.inputs[i][1], dataTraining.inputs[i][2]));
-//   // x1x2x3training.push([x1initial + i, x2initial + i, x3initial + i]);
-// }
-// //
-// console.log(output.toString())
-
-console.log('HERE');
-
 const trainingData = tf.tensor2d(dataTraining.inputs);
 const outputData = tf.tensor1d(dataTraining.outputs);
 const testingData = tf.tensor2d(dataTesting.inputs);
